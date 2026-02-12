8846ef754af81a969d567d784177bb0b7509f455
Simon Gabriel Marian <simon.gabriel.marian@gmail.com>
2026-02-12 11:35:56 +0200

    Initial commit - Playwright OrangeHRM setup

---
+++ tests/example.spec.ts
@@ -0,0 +1,16 @@
+import { test, expect } from '@playwright/test';
+
+test('has login heading', async ({ page }) => {
+  await page.goto('https://opensource-demo.orangehrmlive.com/web/index.php/auth/login');
+
+  // Expect a paragraph to contain login string
+  await expect(page.getByRole('heading', { name: 'Login' })).toHaveText('Login');
+
+  // Expect Username and Password fields to be visible
+  await expect(page.getByPlaceholder('Username')).toBeVisible();
+  await expect(page.getByPlaceholder('Password')).toBeVisible();
+
+  // Expect the Login button to be visible
+  await expect(page.getByRole('button', { name: 'Login' })).toHaveText('Login');
+});
+
