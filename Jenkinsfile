f43c7b97cda94e37cc9e9b38590a5ca1db3a5aea
Simon Gabriel Marian <simon.gabriel.marian@gmail.com>
2026-02-17 12:24:10 +0200

    indentation fixed4

--- Jenkinsfile
+++ Jenkinsfile
@@ -66,7 +66,7 @@ pipeline {
                     alwaysLinkToLastBuild: true,
                     icon:'', 
                     keepAll: true, 
-                    reportDir: 'playwright-report/', 
+                    reportDir: 'playwright-report/html', 
                     reportFiles: 'index.html', 
                     reportName: "Playwright Test Report - Build ${BUILD_NUMBER}", 
                     reportTitles:''
--- playwright.config.ts
+++ playwright.config.ts
@@ -24,7 +24,7 @@ export default defineConfig({
   /* Reporter to use. See https://playwright.dev/docs/test-reporters */
   reporter: [
     ['list'],
-    ['html', { outputFolder: 'playwright-report', open: process.env.CI ? 'never' : 'on-failure' }], 
+    ['html', { outputFolder: 'playwright-report/html', open: process.env.CI ? 'never' : 'on-failure' }], 
     ['junit', { outputFile: 'test-results/results.xml' }]
   ],
   /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
