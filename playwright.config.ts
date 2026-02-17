78758b0238d70f6ab46728dd9d27653024bf6ce1
Simon Gabriel Marian <simon.gabriel.marian@gmail.com>
2026-02-17 12:49:43 +0200

    indentation fixed6

--- playwright.config.ts
+++ playwright.config.ts
@@ -24,8 +24,8 @@ export default defineConfig({
   /* Reporter to use. See https://playwright.dev/docs/test-reporters */
   reporter: [
     ['list'],
-    ['html', { outputFolder: 'playwright-report/html', open: process.env.CI ? 'never' : 'on-failure' }], 
-    ['junit', { outputFile: 'test-results/results.xml' }]
+    ['html', { outputFolder: 'app/playwright-report/html', open: process.env.CI ? 'never' : 'on-failure' }], 
+    ['junit', { outputFile: 'app/test-results/results.xml' }]
   ],
   /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
   use: {
